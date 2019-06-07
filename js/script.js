e139882b64b31ea1f92871e86ad43e74e0d004f9
Bartek Błęcki <bartek@blecki.pl>
2019-06-07 13:49:09 +0200

    Update script.js - add solution

---
+++ js/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var xhr;
+var url = 'http://api.icndb.com/jokes/random';
+
+var button = document.getElementById('get-joke');
+
+button.addEventListener('click', function(){
+    getJoke();
+});
+
+var paragraph = document.getElementById('joke');
+
+function getJoke() {
+    xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.addEventListener('load', function(){
+      var response = JSON.parse(xhr.response);
+      paragraph.innerHTML = response.value.joke;
+    });
+    xhr.send();
+  }
+
+  getJoke();
